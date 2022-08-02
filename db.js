c56e485ea32fac8396c758c0f15065e0871fe0ad
Viseko <viseko23@gmail.com>
2022-08-02 19:46:34 +0700

    Database configurations

---
+++ db.js
@@ -0,0 +1,13 @@
+// Получаем класс пула из пакета PG
+const Pool = requre("pg").Pool;
+// .. и создаём его экземпляр, с пом-ю которого будем делать запросы к БД
+const pool = new Pool({
+  user: "postgres",
+  password: "root", // <- пароль, заданный при установке СУБД
+  host: "localhost",
+  port: 5432,
+  database: "node_postgres" // <- имя БД
+});
+
+// Экспортируем пул для использования в других модулях
+module.exports = pool;
